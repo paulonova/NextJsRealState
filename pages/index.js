b590630ca5598b41ea5f88550a13f9d341f8de0f
paulonova <paulonova144@gmail.com>
2023-08-02 15:38:45 +0200

    First commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client";
+import client from "client";
+
+export default function Home(props) {
+  console.log('Props:', props)
+  return <div>Next JS &amp; WordPress course.</div>;
+}
+
+
+
+export const getStaticProps = async () =>{
+  const {data} = await client.query({
+    query: gql`
+    query NewQuery {
+      pages {
+        nodes {
+          title
+        }
+      }
+    }
+    `
+  })
+  return{
+    props: {
+      data,
+      myexampleprops: 'test'
+    }
+  }
+}
