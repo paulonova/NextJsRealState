d25a646531d8b5caf31576933dc49afccab01423
paulonova <paulonova144@gmail.com>
2023-08-03 16:20:23 +0200

    Versel deployment

--- components/BlockRenderer/index.js
+++ components/BlockRenderer/index.js
@@ -1,11 +1,13 @@
 //export {BlockRenderer} from './BlockRenderer';
 
+import Cover from "components/Cover";
+
 const BlockRenderer = ({ blocks }) => {
 	return (
-		blocks.map((block, index) => {
+		blocks.map((block) => {
 			switch (block.name) {
 				case 'core/cover':
-					return <div key={block.id}>Core Cover</div>;
+					return <Cover key={block.id} background={block.attributes.url}/>;
 				default:
 					return null;
 			}
