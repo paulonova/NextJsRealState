b5896427e2302830bf6a9948f1fc1b0b5290d71f
paulonova <paulonova144@gmail.com>
2023-08-15 15:31:25 +0200

    Creating a ACF Block and set buttons template

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,6 @@
 import { Page } from 'components/Page'
 import { getPageStaticProps } from 'utils/getPageStaticProps'
 
-/**
- * Estou substituindo
- * export default Home=(props)=>{
- * return(
- *  <div>
- *      <MainMenu
- *        item: {props.mainMenu}
- *        callToActionDestination:{props.callToActionDestination}
- *         callToActionLabel:{props.callToActionLabel}
- *      />
- *  </div>
- * )
- * }
- */
-
 export default Page
 
 export const getStaticProps = getPageStaticProps
