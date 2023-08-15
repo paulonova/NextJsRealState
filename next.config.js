0ff7b93a64d68a2a95e338c66f85a081504e4f8d
paulonova <paulonova144@gmail.com>
2023-08-15 15:37:43 +0200

    Fixing Versel

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.NEXT_PUBLIC_WP_IMAGES_URL],
+    domains: [
+      process.env.NEXT_PUBLIC_WP_IMAGES_URL,
+      process.env.NEXT_PUBLIC_BASE_URL,
+    ],
   },
 }
 
