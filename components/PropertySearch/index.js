90bd34ba63d86a9b231610b3394ae4c19dacb315
paulonova <paulonova144@gmail.com>
2023-08-19 19:07:10 +0200

    Create a property card

--- components/PropertySearch/index.js
+++ components/PropertySearch/index.js
@@ -1,18 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Results } from './Results'
 
 const PropertySearch = () => {
+  const [properties, setProperties] = useState([])
+
   useEffect(() => {
     const search = async () => {
       const response = await fetch(`/api/search`)
       const data = await response.json()
       console.log('SEARCH DATA: ', data)
+      setProperties(data.properties)
     }
     search()
   }, [])
   return (
     <div>
-      <Results />
+      <Results properties={properties} />
     </div>
   )
 }
